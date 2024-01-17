571664a25a0e8d7d583a878ff88630828c10631a
judothin <fagrejayden@gmail.com>
2024-01-17 17:34:48 -0500

    commit

--- W2/w2d2 hw/w2d2hw.py
+++ W2/w2d2 hw/w2d2hw.py
@@ -3,16 +3,30 @@
 #prompt: You have been asked to produce a report that lists all the computers in the csv file lab2b.csv. Your
 #report should look like the following sample output. The last line should print the number of
 #computers in the file.
-#Organization of the csv file:
 
 
 import csv
 total_records = 0
 
-with open ('lab2b.csv') as csvfile:
+#idk if this is needed but i added it when i was trying to figure out why it wasnt working
+comp_type = []
+manufacturer = []
+processor = []
+ram = []
+hdd_1 = []
+num_hdd = []
+hdd_2 = []
+os = []
+year = []
+
+import csv
+
+with open ('W2\w2d2 hw\lab2b.csv') as csvfile:
     file = csv.reader(csvfile)
 
-    #for rec 0
+    #labels (yes i looked up how to line them up like this)
+    print("{:<10} {:<10} {:<10} {:<10} {:<10} {:<10} {:<10} {:<10} {:<10}".format("Type", "Man", "Proc", "RAM", "HDD1", "HDD_NUM", "HDD2", "OS", "Year"))
+
     for rec in file:
         if rec[0] == "D":
             comp_type = "Desktop"
@@ -21,22 +35,44 @@ with open ('lab2b.csv') as csvfile:
         else:
             comp_type = "ERROR"
 
-    #for rec 1
         if rec[1] == "DL":
             manufacturer = "Dell"
-        elif rec[1] == "gw":
+        elif rec[1] == "GW":
             manufacturer = "GATEWAY"
         elif rec[1] == "HP":
             manufacturer = rec[1]
         else:
             manufacturer = "ERROR"
 
-    #for all other recs
         processor = rec[2]
         ram = rec[3]
         hdd_1 = rec[4]
         num_hdd = rec[5]
+        os = rec[6]
+        year = rec[7]
 
-    #final printed msg
-        print(rec)
-        print(f"{comp_type}       {manufacturer}        {processor}       {ram}       {hdd_1}       {num_hdd}       {hdd_2}       {os}       {year}")
+        #figuring this out was annoying ngl
+        if num_hdd == "1":
+            hdd_2 = "N/A"
+        elif num_hdd == "2":
+            hdd_2 = rec[6]
+            os = rec[7]
+            year = rec[8]
+        
+        total_records += 1
+
+        
+
+        
+
+    
+
+
+
+        
+
+        #final printed msg
+        print("{:<10} {:<10} {:<10} {:<10} {:<10} {:<10} {:<10} {:<10} {:<10}".format(comp_type, manufacturer, processor, ram, hdd_1, num_hdd, hdd_2, os, year))
+
+#print total amount of records processed
+print(f"Total number of records: {total_records}")
